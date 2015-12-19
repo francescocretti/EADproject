4330e0320b8b57294a9b8aa3a0640dca7ccef9ce
francescocretti <francesco.cretti@gmail.com>
2015-12-19 20:43:09 +0100

    Update segment.m

--- segment.m
+++ segment.m
@@ -1,15 +1,13 @@
+%matlab function for audio segmentation with variable frame size and shift
+%created by Francesco Cretti - December 2015
+
 function [F, N]  = segment(input,frame_size, shift)
 
-%if shift==0
- %   overlap=0;
-    
-%else
-    overlap=frame_size-shift;
-%end
-
+overlap=frame_size-shift;
 frames=buffer(input,frame_size,overlap,'nodelay');
 %cell containing all segments
 F=num2cell(frames,1);
 %number of segments
 N=length(F);
+
 end
