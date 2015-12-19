31bae8e5dafe2db199223218f2dfecdafd503158
francescocretti <francesco.cretti@gmail.com>
2015-12-19 20:46:13 +0100

    Update phase_alignment.m

--- phase_alignment.m
+++ phase_alignment.m
@@ -1,39 +1,56 @@
-%progetto allineamento tracce
+%% Progetto 2016 - Elaborazione del Audio Digitale
 
-%load audio files
-[ref,ref_Fs]=audioread('reference2.wav');
-[test,test_Fs]=audioread('test2.wav');
+% Load audio files
+[ref, Fs, nbits] = wavread('reference2.wav');
+[test, FsT, nbits] = wavread('test2.wav');
 
-subplot(2,1,1);
-plot(ref);
-subplot(2,1,2);
-plot(test);
-%plot waveforms of original files
-% plot(ref);
-% hold on;
-% plot(test);
+% Time vectors
+tr=0:1/Fs:(length(ref)-1)/Fs;
+tt=0:1/Fs:(length(test)-1)/Fs;
 
-%track segmentation
-%frame duration
-fd=4;%4 seconds
-%frame size calc
-fs=fd*ref_Fs;
+% Graph: Ref & Test Signals
+figure
+subplot(2,1,1), plot(tr,ref), ylabel('Ref')
+title('Reference & Test (.wav) Signals')
+subplot(2,1,2), plot(tt,test), xlabel('Time (s)'), ylabel('Test')
 
-%segmentation
+
+%% track segmentation
+fd=4;       %frame duration: 4 seconds
+fs=fd*Fs;   %frame size
+
+% Segmentation
 [refF, refN]=segment(ref,fs,fs);
 [testF, testN]=segment(test,fs,fs);
+i=2;
+% Graph: Segments(i=1)
+figure
+subplot(3,1,1), plot(refF{i}), ylabel('Ref')
+title('First Segments(i=1)')
+subplot(3,1,2), plot(testF{i}), ylabel('Test')
 
-lag=zeros(10,1);
 
-for i=1:refN
- 
-%cross correlation calc
-xc=xcorr(testF{i},refF{i});
-[M,I]=max(xc);
-lag(i)=I;
+%% XCorrelation
+[xc, lag]=xcorr(testF{i},refF{i});
+[M,I]=max(abs(xc));
 
+% Graph Extra: Xcorr
+subplot(3,1,3), plot(lag,abs(xc)), ylabel('XCorr')
+resR=refF{i};
+if lag(I)>0
+    
+    resT=padarray(testF{i}(lag(I)+1:end),[lag(I) 0],'post');
+    
+    %resR=padarray(refF{1},[lag(I) 0],'pre');
+else
+    resT=padarray(testF{i}(1:end-lag(I)-1),[lag(I) 0],'pre');
+    %resR=padarray(refF{1},[lag(I) 0],'post');
 end
-%Steps:
-%calcolo crosscorrelazione segmento per segmento
-%estrazione samples di sfasamento segmento per segmento (plot grafico)
-%compensazione sfasamento
+
+tR=0:1/Fs:(length(resT)-1)/Fs;
+% Graph: Segments(i=1) synchronized
+figure
+subplot(2,1,1), plot(tR,resR), ylabel('Ref'), %axis([3.2 4.5 -0.6 0.6])
+title('First Segments(i=1) synchronized')
+subplot(2,1,2), plot(tR,resT), ylabel('Test'), %axis([3.2 4.5 -0.18 0.18])
+
