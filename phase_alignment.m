482ed63a668fc7e87299a9d27cbec5a60294d4f5
francescocretti <francesco.cretti@gmail.com>
2015-12-20 22:02:13 +0100

    Update phase_alignment.m

--- phase_alignment.m
+++ phase_alignment.m
@@ -1,19 +1,12 @@
 %% Progetto 2016 - Elaborazione del Audio Digitale
 
 % Load audio files
-[ref, Fs, nbits] = wavread('reference2.wav');
-[test, FsT, nbits] = wavread('test2.wav');
+[ref, Fs] = audioread('reference2.wav');
+[test, FsT] = audioread('test2.wav');
 
 % Time vectors
 tr=0:1/Fs:(length(ref)-1)/Fs;
 tt=0:1/Fs:(length(test)-1)/Fs;
-
-% Graph: Ref & Test Signals
-figure
-subplot(2,1,1), plot(tr,ref), ylabel('Ref')
-title('Reference & Test (.wav) Signals')
-subplot(2,1,2), plot(tt,test), xlabel('Time (s)'), ylabel('Test')
-
 
 %% track segmentation
 fd=4;       %frame duration: 4 seconds
@@ -22,21 +15,45 @@ fs=fd*Fs;   %frame size
 % Segmentation
 [refF, refN]=segment(ref,fs,fs);
 [testF, testN]=segment(test,fs,fs);
-i=2;
-% Graph: Segments(i=1)
+
+%initialize vector for lags
+lagVector=zeros(refN,1);
+
+%% XCorrelation calc and plot graphs
+i=1
+%for i=1:refN;
+    
+% Graph: Segments
 figure
 subplot(3,1,1), plot(refF{i}), ylabel('Ref')
-title('First Segments(i=1)')
+string=sprintf('Segment number %d',i);
+title(string)
 subplot(3,1,2), plot(testF{i}), ylabel('Test')
 
-
-%% XCorrelation
+%Xcorrelation
 [xc, lag]=xcorr(testF{i},refF{i});
 [M,I]=max(abs(xc));
-
+lagVector(i)=lag(I);
 % Graph Extra: Xcorr
 subplot(3,1,3), plot(lag,abs(xc)), ylabel('XCorr')
 resR=refF{i};
+%end
+
+%% Plot lags found in each segment
+
+
+% Graph: Ref & Test Signals
+figure
+subplot(3,1,1), plot(tr,ref), ylabel('Ref')
+string=sprintf('Reference & Test (.wav) Signals, with %d seconds segmentation',fd);
+title(string)
+subplot(3,1,2), plot(tt,test), ylabel('Test')
+seg_lags=repelem(lagVector,fd);
+seg_lags=seg_lags ./ Fs;
+subplot(3,1,3), plot(1:(15*fd),seg_lags), xlabel('Time (s)'), ylabel('Lag time')
+
+%%
+
 if lag(I)>0
     
     resT=padarray(testF{i}(lag(I)+1:end),[lag(I) 0],'post');
